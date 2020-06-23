7c8047f943fd09d060595521a997b94d7fcbda0b
hstratton22 <hstrat@byui.edu>
2020-06-23 10:10:47 -0400

    w11

---
+++ W11/script.js
@@ -0,0 +1,10 @@
+function toggleNav() {
+    var updateElement = document.getElementById("menu-icon");
+ //toggle adds a class if it's not there or removes it if it is.
+ updateElement.classList.toggle("open");
+}
+function toggleWrap() {
+    var updateElement = document.getElementById("wrapper");
+ //toggle adds a class if it's not there or removes it if it is.
+ updateElement.classList.toggle("open");
+}       
